0750805fa6a52d8487cecb60c7e6b6de62bda7f6
Edge <777vadimvk@gmail.com>
2018-05-31 01:49:50 +0300

    added time to clockwork

--- app/clock.rb
+++ app/clock.rb
@@ -3,7 +3,7 @@ require './config/environment'
 require 'clockwork'
 
 module Clockwork
-  every(1.day, 'delete.old_urls') {
+  every(1.day, 'delete.old_urls', :at => '00:00') {
     Url.where("created_at <= ?", 15.days.ago.utc).destroy_all
   }
 end
