8fcb102dc456118cabf03f76652d2528639b1565
Edge <777vadimvk@gmail.com>
2018-05-31 00:23:11 +0300

    added devise secret key

---
+++ config/initializers/devise.rb
@@ -0,0 +1,6 @@
+Devise.setup do |config|
+  # The e-mail address that mail will appear to be sent from
+  # If absent, mail is sent from "please-change-me-at-config-initializers-devise@example.com"
+  config.secret_key = ENV['SECRET_KEY_DEVISE']
+
+end
